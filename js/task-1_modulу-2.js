c927fc30b0ce9886a9aa82e0bec9ee02fde355a3
VolodymyrKoch <67268447+VolodymyrKoch@users.noreply.github.com>
2020-09-29 13:33:11 +0300

    start1

---
+++ js/task-1_modulу-2.js
@@ -0,0 +1,18 @@
+// // -----task-6----
+// let total = 0
+// do {
+//   let input = prompt(`Введи число`)
+//   if (input === null) {
+//     console.log(`Відмінено користувачем`)
+//     break
+//   }
+//   input = Number(input)
+//   const notANumber = Number.isNaN(input)
+//   if (notANumber) {
+//     console.log(`Введено не число, пропускаємо даний крок циклу`)
+//     continue
+//   }
+//   total += input
+// } while (true)
+// console.log(`загальна сума ${total}`)
+// // ----------------------------------------------------
